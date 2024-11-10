4f69c56fcdf55d630d0ea28bc859093641671e49
Boshi Lian <farmer1992@gmail.com>
2024-11-10 03:05:11 -0800

    Update dependencies and enhance e2e test for key-based authentication

--- database/e2e_test.go
+++ database/e2e_test.go
@@ -317,92 +317,92 @@ func TestSqliteDatabase(t *testing.T) {
 		testcommon.CheckSharedFileContent(t, targetfie, randtext)
 	})
 
-	// t.Run("keytopass", func(t *testing.T) {
+	t.Run("keytopass", func(t *testing.T) {
 
-	// 	if err := testcommon.RunCmdAndWait("rm", "-f", path.Join(testdir, "keypass")); err != nil {
-	// 		t.Errorf("failed to remove id_rsa: %v", err)
-	// 	}
+		if err := testcommon.RunCmdAndWait("rm", "-f", path.Join(testdir, "keypass")); err != nil {
+			t.Errorf("failed to remove id_rsa: %v", err)
+		}
 
-	// 	if err := testcommon.RunCmdAndWait(
-	// 		"ssh-keygen",
-	// 		"-N",
-	// 		"",
-	// 		"-f",
-	// 		path.Join(testdir, "keypass"),
-	// 	); err != nil {
-	// 		t.Errorf("failed to generate private key: %v", err)
-	// 	}
+		if err := testcommon.RunCmdAndWait(
+			"ssh-keygen",
+			"-N",
+			"",
+			"-f",
+			path.Join(testdir, "keypass"),
+		); err != nil {
+			t.Errorf("failed to generate private key: %v", err)
+		}
 
-	// 	pk, err := os.ReadFile(path.Join(testdir, "keypass.pub"))
-	// 	if err != nil {
-	// 		t.Errorf("failed to read public key: %v", err)
-	// 	}
+		pk, err := os.ReadFile(path.Join(testdir, "keypass.pub"))
+		if err != nil {
+			t.Errorf("failed to read public key: %v", err)
+		}
 
-	// 	sqlite3db.Create(&downstream{
-	// 		Username:    "keypass",
-	// 		AuthMapType: authMapTypePrivateKey,
-	// 		AuthorizedKeys: keydata{
-	// 			Data: string(pk),
-	// 		},
-	// 		Upstream: upstream{
-	// 			Username:    "user",
-	// 			AuthMapType: authMapTypePassword,
-	// 			Password:    "pass",
-	// 			Server: server{
-	// 				Address: "host-password:2222",
-	// 				HostKey: keydata{
-	// 					Data: string(knownHostsPassData),
-	// 				},
-	// 			},
-	// 		},
-	// 	})
+		sqlite3db.Create(&downstream{
+			Username:    "keypass",
+			AuthMapType: authMapTypePrivateKey,
+			AuthorizedKeys: keydata{
+				Data: string(pk),
+			},
+			Upstream: upstream{
+				Username:    "user",
+				AuthMapType: authMapTypePassword,
+				Password:    "pass",
+				Server: server{
+					Address: "host-password:2222",
+					HostKey: keydata{
+						Data: string(knownHostsPassData),
+					},
+				},
+			},
+		})
 
-	// 	piperaddr, piperport := testcommon.NextAvailablePiperAddress()
+		piperaddr, piperport := testcommon.NextAvailablePiperAddress()
 
-	// 	piper, _, _, err := testcommon.RunCmd("/sshpiperd/sshpiperd",
-	// 		"-p",
-	// 		piperport,
-	// 		"/sshpiperd/plugins/database",
-	// 		"--driver",
-	// 		"sqlite3",
-	// 		"--sqlite-file",
-	// 		dbfile,
-	// 	)
+		piper, _, _, err := testcommon.RunCmd("/sshpiperd/sshpiperd",
+			"-p",
+			piperport,
+			"/sshpiperd/plugins/database",
+			"--driver",
+			"sqlite3",
+			"--sqlite-file",
+			dbfile,
+		)
 
-	// 	if err != nil {
-	// 		t.Errorf("failed to run sshpiperd: %v", err)
-	// 	}
+		if err != nil {
+			t.Errorf("failed to run sshpiperd: %v", err)
+		}
 
-	// 	defer testcommon.KillCmd(piper)
+		defer testcommon.KillCmd(piper)
 
-	// 	testcommon.WaitForEndpointReady(piperaddr)
+		testcommon.WaitForEndpointReady(piperaddr)
 
-	// 	randtext := uuid.New().String()
-	// 	targetfie := uuid.New().String()
+		randtext := uuid.New().String()
+		targetfie := uuid.New().String()
 
-	// 	c, _, _, err := testcommon.RunCmd(
-	// 		"ssh",
-	// 		"-v",
-	// 		"-o",
-	// 		"StrictHostKeyChecking=no",
-	// 		"-p",
-	// 		piperport,
-	// 		"-l",
-	// 		"keypass",
-	// 		"-i",
-	// 		path.Join(testdir, "keypass"),
-	// 		"127.0.0.1",
-	// 		fmt.Sprintf(`sh -c "echo -n %v > /shared/%v"`, randtext, targetfie),
-	// 	)
+		c, _, _, err := testcommon.RunCmd(
+			"ssh",
+			"-v",
+			"-o",
+			"StrictHostKeyChecking=no",
+			"-p",
+			piperport,
+			"-l",
+			"keypass",
+			"-i",
+			path.Join(testdir, "keypass"),
+			"127.0.0.1",
+			fmt.Sprintf(`sh -c "echo -n %v > /shared/%v"`, randtext, targetfie),
+		)
 
-	// 	if err != nil {
-	// 		t.Errorf("failed to ssh to %v", err)
-	// 	}
+		if err != nil {
+			t.Errorf("failed to ssh to %v", err)
+		}
 
-	// 	defer testcommon.KillCmd(c)
+		defer testcommon.KillCmd(c)
 
-	// 	time.Sleep(time.Second)
+		time.Sleep(time.Second)
 
-	// 	testcommon.CheckSharedFileContent(t, targetfie, randtext)
-	// })
+		testcommon.CheckSharedFileContent(t, targetfie, randtext)
+	})
 }
--- go.mod
+++ go.mod
@@ -2,18 +2,19 @@ module github.com/tg123/sshpiper-plugins
 
 go 1.23.2
 
-replace golang.org/x/crypto => github.com/tg123/sshpiper.crypto v0.27.0-sshpiper-20240908
+replace golang.org/x/crypto => github.com/tg123/sshpiper.crypto v0.29.0-sshpiper-20241110
 
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.16.0
 	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.8.0
-	github.com/creack/pty v1.1.23
+	github.com/creack/pty v1.1.24
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/google/uuid v1.6.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/microsoft/kiota-authentication-azure-go v1.1.0
 	github.com/microsoftgraph/msgraph-sdk-go v1.51.0
 	github.com/sirupsen/logrus v1.9.3
-	github.com/tg123/sshpiper v1.4.3
+	github.com/tg123/sshpiper v1.4.4
 	github.com/urfave/cli/v2 v2.27.5
 )
 
@@ -28,7 +29,6 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/lib/pq v1.1.1 // indirect
@@ -51,12 +51,12 @@ require (
 	go.opentelemetry.io/otel v1.31.0 // indirect
 	go.opentelemetry.io/otel/metric v1.31.0 // indirect
 	go.opentelemetry.io/otel/trace v1.31.0 // indirect
-	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241007155032-5fefd90f89a9 // indirect
-	google.golang.org/grpc v1.67.1 // indirect
+	google.golang.org/grpc v1.68.0 // indirect
 	google.golang.org/protobuf v1.35.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
